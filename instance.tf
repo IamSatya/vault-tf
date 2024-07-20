0edb93194f551f5e5fee6cf5237182898fdc12e2
SatyaPanuganti <satyapanuganti@gmail.com>
2024-07-20 18:07:35 +0530

    adding sg rule

--- instance.tf
+++ instance.tf
@@ -23,7 +23,8 @@ resource "aws_instance" "vault" {
         "gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint",
         "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/hashicorp.list",
         "sudo apt update",
-        "sudo apt install vault -y"
+        "sudo apt install vault -y",
+        "vault server -dev -dev-listen-address=\"0.0.0.0:8200\" & > ~/vault.out"
      ]
     }
     depends_on = [ aws_key_pair.vaultkey, aws_security_group.ssh ]
--- sg.tf
+++ sg.tf
@@ -14,6 +14,14 @@ resource "aws_security_group" "ssh" {
         ipv6_cidr_blocks = ["::/0"]
     }
 
+    ingress {
+        from_port = 8200
+        to_port = 8200
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0

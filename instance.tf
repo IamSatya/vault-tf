7faaaa9598ce262351fccb6143797ac07463f34d
SatyaPanuganti <satyapanuganti@gmail.com>
2024-07-20 16:49:57 +0530

    adding vault server with install provisioners

---
+++ instance.tf
@@ -0,0 +1,28 @@
+resource "aws_key_pair" "vaultkey" {
+  key_name = "vaultkey"
+  public_key = file("/root/.ssh/awskey.pub") 
+}
+
+resource "aws_instance" "vault" {
+  ami = "ami-ininnvnv"
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.vaultkey
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("/root/.ssh/awskey.pub")
+    host = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "sudo apt update && sudo apt install gpg -y",
+        "wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg",
+        "gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint",
+        "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/hashicorp.list",
+        "sudo apt update",
+        "sudo apt install vault -y"
+     ]
+    }
+}
